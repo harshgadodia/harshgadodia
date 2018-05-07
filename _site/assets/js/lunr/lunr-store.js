389ce12099d03722d53d689e13d6f1ee71c7c384
Harsh Gadodia <harsh@u.nus.edu>
2018-05-08 01:44:42 +0800

    add about, contact and first post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "Hello, World",
+        "excerpt":"Site in progress I am in the process of building this website. Some links may or may not work while I am messing around with stuff - apologies, and I hope you revisit after I’m up and running! ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/Hello,-World/",
+        "teaser":null}]
