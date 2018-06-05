be21b7c00371b6ba63bc4c84df7db55678e6179c
Harsh Gadodia <harsh@u.nus.edu>
2018-06-06 01:42:58 +0800

    make latest post wider

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"Testing Uploading Files You can download the keynote document here directly. ","categories": ["writings"],
         "tags": [],
         "url": "http://localhost:4000/writings/Testing-Uploading/",
+        "teaser":null},{
+        "title": "The Bae Area",
+        "excerpt":"I had the opportunity to spend 2017 living and working in the San Francisco Bay Area, California, and it was definitely one of the greatest personal and professional experiences of my life. Being in the Bay Area was refreshing - you surely learn a great deal about yourself when you’re...","categories": ["writings"],
+        "tags": [],
+        "url": "http://localhost:4000/writings/The-Bae-Area/",
         "teaser":null}]
